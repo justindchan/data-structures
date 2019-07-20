07d3c1087324aa28bd52e86d3e56bf88fc56419c
Hack Reactor Student <student@hackreactor.com>
2019-07-20 12:12:06 -0700

    Completed tree - all test passed

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,9 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+  
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,9 +13,35 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  // i: target ; o: boolean
+  var bool = false;
+  // current children to loop through = this.children
+  var current = this.children;
+  // create helper function to recurse over
+  var finder = function (current) {
+    // loop through current children
+    for (let i = 0; i < current.length; i++) {
+      // assign current children's children to current node
+      var node = current[i];
+      if (node.value === target) {
+        bool = true;
+      }
+      finder(node.children);
+    }
+  };
+  finder(current);
+  return bool;
+  //base case
+    // if tree.value === target
+    // bool = true
+  
+  // recursive case
+    // is newTree.children.length !== 0?
+    // if yes, recurse newTree.children
 };
 
 
