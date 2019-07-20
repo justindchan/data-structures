7e4a93d7a4ff08fcdabfe938ff6e6a4c35bfe915
Hack Reactor Student <student@hackreactor.com>
2019-07-19 22:21:47 -0700

    Completed set - passed all test

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,35 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  // if item is not present in set,
+  var found = this.contains(item);
+  if (found === false) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  var found = false;
+  for (let i = 0; i < this._storage.length; i++) {
+    if (this._storage[i] === item) {
+      return true;
+    } 
+  }
+  return found;
 };
 
 setPrototype.remove = function(item) {
+  for (let i = 0; i < this._storage.length; i++) {
+    if (this._storage[i] === item) {
+      this._storage.splice(i, 1);
+    }
+  }
 };
 
 /*
